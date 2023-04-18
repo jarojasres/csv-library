180d7abbde9f1fcb73c649b47afcde1fdace0c74
jarojasres <jarojasres@gmail.com>
2023-04-18 08:40:44 -0500

    Add test file

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export const chunk = (arr: Array<any>, size: number) => {
+  return Array.from({ length: Math.ceil(arr.length / size) }, (_, i) =>
+    arr.slice(i * size, i * size + size)
+  );
+};

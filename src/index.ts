71449ece654a3e04fb83b180cede8774f236bee6
jarojasres <jarojasres@gmail.com>
2023-04-19 21:48:32 -0500

    update workflow

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export const chunk = (arr: Array<any>, size: number) => {
   console.log("esto es una prueba");
+  console.log("esto es una prueba 2");
   return Array.from({ length: Math.ceil(arr.length / size) }, (_, i) =>
     arr.slice(i * size, i * size + size)
   );
